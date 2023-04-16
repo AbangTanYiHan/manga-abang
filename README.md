e23db1c4bfdc7d844ebdcecb4ed8f6771456db57
abangtan <shinichiconan1997@gmail.com>
2023-04-16 15:50:51 +0900

    update setup

---
+++ README.md
@@ -0,0 +1,21 @@
+# TL
+
+## Installation
+Run in command line:
+
+```commandline
+pip3 install tl
+```
+
+## Running in the background
+
+```commandline
+tl -w <your txt file path>
+```
+
+## Usage
+z key: page + 1
+spacebar key: picture + 1
+b key: bubblt + 1
+x key: box + 1
+t key: text + 1
--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ from setuptools import setup
 long_description = (Path(__file__).parent / "README.md").read_text('utf-8').split('# Installation')[0]
 
 setup(
-    name="tl",
-    version='0.1.8',
+    name="tl_typewriter",
+    version='0.1',
     description="pagination and bubble typewriter in translating manga",
-    url="https://github.com/kha-white/manga-ocr",
+    long_description=long_description,
+    url="https://github.com/AbangTanYiHan/tl",
     author="abangtan",
     license="Apache License 2.0",
     classifiers=[
@@ -22,7 +23,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-        'tl = tl:main'
+        'tl = tl.tl:main'
         ]
     },
 )
